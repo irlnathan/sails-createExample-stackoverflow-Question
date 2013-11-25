46064bade746cf10a408320ce343ebb62dc89477
irlnathan <irlnathan@gmail.com>
2013-11-25 09:42:30 -0600

    initial commit

---
+++ api/controllers/UserController.js
@@ -0,0 +1,44 @@
+/**
+ * UserController
+ *
+ * @module      :: Controller
+ * @description	:: A set of functions called `actions`.
+ *
+ *                 Actions contain code telling Sails how to respond to a certain type of request.
+ *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+ *
+ *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+ *                 and/or override them with custom routes (`config/routes.js`)
+ *
+ *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
+ *
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+module.exports = {
+
+	"new": function(req, res) {
+		res.view();
+	},
+    
+	create: function (req, res, next) {
+
+	    User.create(req.params.all()).done(function (err, user) {
+	        if ( err ) {
+	            return next(err);
+	        }
+	        else {
+	            res.json(user);
+	        }
+	    });
+	},    
+
+
+  /**
+   * Overrides for the settings in `config/controllers.js`
+   * (specific to UserController)
+   */
+  _config: {}
+
+  
+};
